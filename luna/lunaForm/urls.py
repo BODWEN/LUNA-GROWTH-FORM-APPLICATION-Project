67a3894234504c26d4acbb919821600577b615ef
BODWEN <hassansadiqb217@gmail.com>
2024-07-30 01:05:08 +0100

    work in progress

--- luna/lunaForm/urls.py
+++ luna/lunaForm/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('applicant/', views.applicant, name='applicant'),
+    path('guarantor/', views.guarantor, name='guarantor'),
 ]
